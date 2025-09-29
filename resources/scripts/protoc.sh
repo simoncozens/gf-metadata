4967d752c5c290824d3cd55e9322c3eddda91d7c
Simon Cozens <simon@simon-cozens.org>
2025-09-29 11:58:32 +0100

    Update script

--- resources/scripts/protoc.sh
+++ resources/scripts/protoc.sh
@@ -3,18 +3,13 @@
 # Mac: brew install protoc
 # Many Linux: sudo apt-get install protoc
 
-# assumes the following clones:
-# ~/oss/gftools is https://github.com/googlefonts/gftools
-# ~/oss/fonts is https://github.com/google/fonts
+export GFTOOLS="${GFTOOLS:-$HOME/oss/gftools}"
 
 cargo install protobuf-codegen
 PATH="$HOME/.cargo/bin:$PATH"
 
-protoc --rs_out gf-metadata/src/ --proto_path ~/oss/gftools/Lib/gftools/ ~/oss/gftools/Lib/gftools/fonts_public.proto
+protoc --rs_out src/ --proto_path $GFTOOLS/Lib/gftools $GFTOOLS/Lib/gftools/fonts_public.proto
+protoc --rs_out src/ --proto_path $GFTOOLS/Lib/gftools $GFTOOLS/Lib/gftools/designers.proto
+protoc --rs_out src/ --proto_path $GFTOOLS/Lib/gftools $GFTOOLS/Lib/gftools/axes.proto
 
-protoc --rs_out gf-metadata/src/ --proto_path ~/oss/fonts/lang/Lib/gflanguages/ ~/oss/fonts/lang/Lib/gflanguages/languages_public.proto
-
-protoc --rs_out gf-embed/src/ --proto_path resources/scripts/ resources/scripts/embed_data.proto
-
-rm gf-metadata/src/mod.rs
-rm gf-embed/src/mod.rs
+rm src/mod.rs
