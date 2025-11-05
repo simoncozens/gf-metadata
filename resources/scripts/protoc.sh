4a2e5c2f0d6337ecb36982722c56fa6d39127fa1
Simon Cozens <simon@simon-cozens.org>
2025-11-05 10:20:46 +0000

    Add all known protos, compile them locally

--- resources/scripts/protoc.sh
+++ resources/scripts/protoc.sh
@@ -3,13 +3,11 @@
 # Mac: brew install protoc
 # Many Linux: sudo apt-get install protoc
 
-export GFTOOLS="${GFTOOLS:-$HOME/oss/gftools}"
-
 cargo install protobuf-codegen
 PATH="$HOME/.cargo/bin:$PATH"
 
-protoc --rs_out src/ --proto_path $GFTOOLS/Lib/gftools $GFTOOLS/Lib/gftools/fonts_public.proto
-protoc --rs_out src/ --proto_path $GFTOOLS/Lib/gftools $GFTOOLS/Lib/gftools/designers.proto
-protoc --rs_out src/ --proto_path $GFTOOLS/Lib/gftools $GFTOOLS/Lib/gftools/axes.proto
+mkdir -p Lib/gfmetadata/
+
+protoc -I resources/protos --rs_out src/ --python_out Lib/gfmetadata resources/protos/*.proto
 
 rm src/mod.rs
