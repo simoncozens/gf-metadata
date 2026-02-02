22f55cf1f3ce5854f8c5c812d977f99fd4b24921
Simon Cozens <simon@simon-cozens.org>
2026-02-02 11:33:50 +0000

    Import and re-export all the protos

---
+++ Lib/gfmetadata/__init__.py
@@ -0,0 +1,30 @@
+# Re-export all the things for convenience
+from gfmetadata.axes_pb2 import AxisProto, FallbackProto
+from gfmetadata.designers_pb2 import DesignerInfoProto, AvatarProto
+from gfmetadata.fonts_public_pb2 import (
+    AxisSegmentProto,
+    FamilyFallbackProto,
+    FamilyProto,
+    FontProto,
+    GlyphGroupProto,
+    SampleTextProto,
+    SourceFileProto,
+    SourceProto,
+    TargetProto,
+    TargetTypeProto,
+)
+from gfmetadata.knowledge_pb2 import (
+    ContributorsProto,
+    KnowledgeProto,
+    LessonProto,
+    ModuleProto,
+    TermProto,
+    TopicProto,
+)
+from gfmetadata.languages_public_pb2 import (
+    ExemplarCharsProto,
+    LanguageProto,
+    RegionProto,
+    SampleTextProto,
+    ScriptProto,
+)
